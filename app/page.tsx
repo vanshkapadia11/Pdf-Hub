645214aa80597a6010916f3a82181aa251da2dcd
Vansh Kapadia <144871405+vanshkapadia11@users.noreply.github.com>
2025-09-13 17:16:58 +0530

    SEO Thing Going On!

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import LandingPage from "./pages/dashboard/page";
+import LandingPage from "./dashboard/page";
 
 const page = () => {
   return (
